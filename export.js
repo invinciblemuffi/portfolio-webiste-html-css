87fc2b98b52354782717675fbec73ba33ec67eef
Mufaddal Barwaniwala <invinciblemuffi@gmail.com>
2021-11-01 15:54:31 +0530

    Portfolio website code

---
+++ export.js
@@ -0,0 +1,11 @@
+// ES6 import export
+
+export const PI = Math.PI;
+
+export default function cube(num) {
+  return num * num * num;
+}
+
+export function findMax(arrNum) {
+  return Math.max(...arrNum);
+}
---
+++ import.js
@@ -0,0 +1,9 @@
+import cube, { findMax } from "./export.js";
+import { PI } from "./export.js";
+
+var cubeVal = cube(3);
+console.log(cubeVal);
+console.log(PI);
+
+var maxValue = findMax([-50, 2, 190, 0, -9]);
+console.log(maxValue);
