24eff37570af9eea08dabb6690e8489500b8d25c
Mufaddal B <invinciblemuffi@gmail.com>
2022-03-31 16:46:14 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+// Type Annotation on variables
+
 let numA:number = 10
 let strB: string = 'My Name is: '
 let arrX: number[] = [4,5,7]
@@ -8,3 +10,5 @@ let isDone: boolean = true
 let tupleZ: [number, string, boolean]
 tupleZ = [10, 'Match the string type', true] // Valid
 tupleZ = [10, 20, true] // Error: Type 'number' is not assignable to type 'string'.
+
+// Return Type Annotations
